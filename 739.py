d20deae3a51b13defdadb869b7dc3fb47acc78d2
BillUtada <william.zhao.programmer@gmail.com>
2018-03-21 15:10:15 +0800

    add solution

--- 739.py
+++ 739.py
@@ -13,3 +13,16 @@ class Solution(object):
             stack.append(i);
         return res;
 
+	'''
+        res = [0]*len(temperatures);
+        dic = {};
+        for i in range(len(temperatures))[::-1]:
+            nearest = len(temperatures);
+            for temp in range(temperatures[i]+1, 101):
+                if temp in dic and dic[temp] < nearest:
+                    nearest = dic[temp];
+            if nearest != len(temperatures):
+                res[i] = nearest-i;
+            dic[temperatures[i]] = i;
+        return res;
+        '''
